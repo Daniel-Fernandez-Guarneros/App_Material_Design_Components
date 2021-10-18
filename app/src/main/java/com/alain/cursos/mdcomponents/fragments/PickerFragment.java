b0018b589e97b0e50b50108a8b38e1a42df563a5
Daniel Fernandez Guarneros <danierse63@gmail.com>
2021-10-18 13:40:31 -0500

    024 Picker_ Dialog

--- app/src/main/java/com/alain/cursos/mdcomponents/fragments/PickerFragment.java
+++ app/src/main/java/com/alain/cursos/mdcomponents/fragments/PickerFragment.java
@@ -2,18 +2,21 @@ package com.alain.cursos.mdcomponents.fragments;
 
 
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.alain.cursos.mdcomponents.R;
 import com.alain.cursos.mdcomponents.utils.Component;
 import com.alain.cursos.mdcomponents.utils.Constants;
+import com.google.android.material.datepicker.MaterialDatePicker;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import butterknife.Unbinder;
 
 /**
@@ -26,6 +29,8 @@ public class PickerFragment extends Fragment {
     private static Component mInstance;
 
     Unbinder mUnbinder;
+    @BindView(R.id.containerMain)
+    LinearLayout containerMain;
 
     public static Component getmInstance() {
         mInstance = new Component();
@@ -48,7 +53,6 @@ public class PickerFragment extends Fragment {
         mUnbinder = ButterKnife.bind(this, view);
 
 
-
         return view;
     }
 
@@ -56,5 +60,20 @@ public class PickerFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         mUnbinder.unbind();
+    }
+
+    @OnClick({R.id.btnDialog, R.id.btnFullscreen})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.btnDialog:
+                MaterialDatePicker.Builder<Long> builder = MaterialDatePicker.Builder.datePicker();
+                builder.setTitleText(R.string.picker_title);
+                builder.setTheme(R.style.ThemeOverlay_MaterialComponents_MaterialCalendar);
+                MaterialDatePicker<?> picker = builder.build();
+                picker.show(getFragmentManager(), picker.toString());
+                break;
+            case R.id.btnFullscreen:
+                break;
+        }
     }
 }
